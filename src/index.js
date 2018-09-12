5c739f84990a33647ec01c997be37ad370bfd26e
Dan Junger <junger.da@gmail.com>
2018-09-12 11:22:25 -0700

    add airbnb linting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import {updateIn} from 'updeep'
+import { updateIn } from 'updeep';
 
-export const SET_STATE = 'SET_STATE'
+export const SET_STATE = 'SET_STATE';
 
 export function setState(key, payload) {
   return {
     type: SET_STATE,
     key,
-    payload
-  }
+    payload,
+  };
 }
 
 export function createReducer(initialState) {
   return function reducer(state = initialState, action) {
     if (action.type === SET_STATE) {
-      return updateIn(action.key, action.payload, state)
+      return updateIn(action.key, action.payload, state);
     }
 
-    return state
-  }
+    return state;
+  };
 }
-
